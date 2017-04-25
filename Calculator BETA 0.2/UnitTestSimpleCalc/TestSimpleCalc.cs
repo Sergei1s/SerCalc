405840891a5eb5be6965e32a70d1ba1990a52bc0
Sergei1s <Sergeikazankov12@gmail.com>
2017-04-25 17:34:40 +0400

    BETA 0.2.new

---
+++ Calculator BETA 0.2/UnitTestSimpleCalc/TestSimpleCalc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator1;
+
+namespace UnitTestSimple
+
+{
+    [TestClass]
+    public class TestSimpleCalc
+    {
+        [TestMethod]
+        public void addition()
+        {
+            Calculator Calc = new Calculator();
+            float result = Calc.Plus(10, 20);
+            Assert.AreEqual(30, result);
+        }
+
+        [TestMethod]
+        public void subtraction()
+        {
+            Calculator Calc = new Calculator();
+            float result = Calc.Minus(25, 5);
+            Assert.AreEqual(20, result);
+        }
+
+        
+
+
+        [TestMethod]
+        public void division()
+        {
+            Calculator Calc = new Calculator();
+            float result = Calc.Dividing(56, 8);
+            Assert.AreEqual(result, 7);
+
+        }
+
+
+
+
+
+
+        [TestMethod]
+        public void multiplication()
+        {
+            Calculator Calc = new Calculator();
+            float result = Calc.Multiply(5, 8);
+            Assert.AreEqual(40, result);
+        }
+    }
+}
+    
+
+
