b8aa153adad653411a4115accda53466e9d82184
Sergei1s <Sergeikazankov12@gmail.com>
2017-04-25 23:04:25 +0400

    BETA 0.2.1

---
+++ Calculator BETA 0.2/Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Calculator1
+
+{
+    public class Calculator
+    {
+        private char _oper;
+
+
+        public float Plus(float x, float y)
+        {
+            return x + y;
+        }
+
+        public float Minus(float x, float y)
+        {
+            return x - y;
+        }
+
+        public float Multiply(float x, float y)
+        {
+            return x * y;
+        }
+
+        public float Dividing(float x, float y)
+        {
+            return x / y;
+        }
+
+        private class ConsoleManager
+        {
+        }
+
+        private class Program
+        {
+            private static void Main(string[] args)
+            {
+            }
+        }
+    }
+}
