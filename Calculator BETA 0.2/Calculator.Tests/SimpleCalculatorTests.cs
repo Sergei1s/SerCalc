39fd922d7d98abbdc214f2dea8b4a897d937a9ff
Sergei1s <Sergeikazankov12@gmail.com>
2017-05-11 19:41:01 +0400

    Переименование методов

--- Calculator BETA 0.2/Calculator.Tests/SimpleCalculatorTests.cs
+++ Calculator BETA 0.2/Calculator.Tests/SimpleCalculatorTests.cs
@@ -1,49 +1,46 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Calculator1;
 
-namespace UnitTestSimple
-
+namespace Calculator.Tests
 {
     [TestClass]
-    public class TestSimpleCalc
+    public class SimpleCalculatorTests
     {
         [TestMethod]
         public void  ShouldPlusTwoVariablesTest()
         {
-            var calc = new Calculator();
-            var result = calc.Plus(10, 20);
+            var calculator = new Calculator1.Calculator();
+            var result = calculator.Plus(10, 20);
             Assert.AreEqual(30, result);
         }
 
         [TestMethod]
         public void ShouldSubtractTwoVariablesTest()
         {
-            var calc = new Calculator();
-            var result = calc.Minus(25, 5);
+            var calculator = new Calculator1.Calculator();
+            var result = calculator.Minus(25, 5);
             Assert.AreEqual(20, result);
         }
 
         [TestMethod]
-        public void SouldDivideTwoVariablesTest()
+        public void ShouldDivideTwoVariablesTest()
         {
-            var calc = new Calculator();
-            var result = calc.Divide(56, 8);
+            var calculator = new Calculator1.Calculator();
+            var result = calculator.Divide(56, 8);
             Assert.AreEqual(result, 7);
         }
         [TestMethod]
         [ExpectedException(typeof(DivideByZeroException))]
         public void ShouldRaiseExeption_WhenDeliverEqualsZero_Test()
         {
-            var calc = new Calculator();
+            var calc = new Calculator1.Calculator();
             var result = calc.Divide(56, 0);
         }
 
         [TestMethod]
         public void ShouldMultiplyTwoVariablesTest()
         {
-            var calc = new Calculator();
+            var calc = new Calculator1.Calculator();
             var result = calc.Multiply(5, 8);
             Assert.AreEqual(40, result);
         }
